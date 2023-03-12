b0cec346e1915b9e080552d59267ebd3ab87582b
Dean <dean@dpb.pw>
2023-03-11 19:15:39 -0800

    UUID Updates

--- db/store.js
+++ db/store.js
@@ -1,6 +1,6 @@
 const util = require('util');
 const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('../node_modules/uuidv4');
 
 const readNote = util.promisify(fs.readFile);
 const writeNote = util.promisify(fs.writeFile);
